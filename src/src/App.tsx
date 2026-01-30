8027e6989c59d871454a3b4292e9f2d9e90d8277
Rival_X <edwinfello50@gmail.com>
2026-01-30 21:29:54 +0100

    Delete src/src directory

--- src/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>Gigaverse Fish Assist ✅</h1>
-      <p>If you can see this, React + Vite + Pages is working.</p>
-    </div>
-  );
-}
