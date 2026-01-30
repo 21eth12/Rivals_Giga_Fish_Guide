2c3edc8bce1fcb19cdeef7ebf54c28b57d2b9459
Rival_X <edwinfello50@gmail.com>
2026-01-30 21:33:19 +0100

    Create initial App component with basic layout

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui", color: "white" }}>
+      <h1>Gigaverse Fish Assist</h1>
+      <p>If you can see this, React is working ✅</p>
+    </div>
+  );
+}
